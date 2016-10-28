da4978c5fb3b8f063c63294037e93ebe5b9d6b19
Larothus <larothus@gmail.com>
2016-10-28 23:45:36 +0200

    Complete Rewrite
    • The plot_tools script not takes a standard .ini file as config file.
    • The plot_tools was rewritten to accept an infinite number of inputs.

--- plot_tools.py
+++ plot_tools.py
@@ -13,7 +13,8 @@
 #		MODULES
 #===============================================
 
-from numpy import array
+from ConfigParser import SafeConfigParser
+from numpy import array,genfromtxt
 from matplotlib import pyplot
 from os import path,mkdir
 
@@ -25,98 +26,54 @@ from os import path,mkdir
 #		OBJECTS AND FUNCTIONS
 #===============================================
 
-def read_config(properties):
-	out = {}
-	with open(properties) as in_file:
-		for line in in_file:
-			if line[0]=='\n' or line[0]=='#': continue
-			(key, val) = line.split(':')
-			out[key] = val[:-1].strip()
-	return out
+def plot(properties):
+	"""Plots an x vs y plot from any number of csv files. Uses as input the data given in the properties file."""
 
-def plot_single(properties):
-	"""Plots an x vs y plot from two csv files. Uses as input the data given in the properties file."""
-
-	properties = read_config(properties)
-
-	#++++++++++ DATA ++++++++++
-	infile = genfromtxt(properties['infile'],delimiter=',',names=True,dtype=None,comments='#',missing_values='None')
-	x_data,y_data = infile[properties['x,y'].split(',')[0]],infile[properties['x,y'].split(',')[1]]
-
-	#++++++++++ LINE FORMAT ++++++++++
-	if properties['size'] != 'False': pyplot.figure(num=1,figsize=eval(properties['size']),dpi=eval(properties['dpi']))
-	else: pyplot.figure(num=1,dpi=eval(properties['dpi']))
-	pyplot.clf()
-	pyplot.subplot(1,1,1)
-	p, = pyplot.plot(x_data,y_data,marker=properties['marker'],markersize=int(properties['markersize']),markeredgewidth=float(properties['markeredgewidth']),linestyle=properties['linestyle'],color=array(eval(properties['color']))/255,alpha=float(properties['alpha']))
+	config = SafeConfigParser()
+	config.read(properties)
 
 	#++++++++++ PLOT FORMAT ++++++++++
-	pyplot.suptitle(properties['suptitle'],fontsize=16)
-	pyplot.figtext(0.5,0.05,properties['x_label'],ha='center',va='center')
-	pyplot.figtext(0.05,0.5,properties['y_label'],rotation=90,ha='center',va='center')
-	pyplot.xticks(fontsize=8)
-	pyplot.yticks(fontsize=8)
-	if properties['x_lim'] != 'False':
-		xlim = eval(properties['x_lim'])
-		pyplot.xlim(xlim[0],xlim[1])
-	if properties['y_lim'] != 'False':
-		ylim = eval(properties['y_lim'])
-		pyplot.ylim(ylim[0],ylim[1])
-	if properties['invert_x'] == 'True': pyplot.gca().invert_xaxis()
-	if properties['invert_y'] == 'True': pyplot.gca().invert_yaxis()
-	if properties['grid'] != 'False': pyplot.grid(b='on',which='major',axis=properties['grid'])
-	if properties['equalize_grid'] == 'True': pyplot.gca().set_aspect('equal', adjustable='box')
-
-	#++++++++ SAVE PLOT ++++++++
-	if properties['outfile'] == 'False': outfile = properties['infile'].split('.')[0]+'.pdf'
-	else: outfile = properties['outfile']
-	outpath = properties['outpath']
-	if not path.exists(outpath): mkdir(outpath)
-	pyplot.savefig('%s%s' %(outpath,outfile))
-	print('Plot saved to %s%s' %(outpath,outfile))
-	pyplot.close()
-
-def plot_double(properties):
-	"""Plots an x vs y plot from two csv files. Uses as input the data given in the properties file."""
-
-	properties = read_config(properties)
-
-	#++++++++++ DATA ++++++++++
-	infile1 = genfromtxt(properties['infile1'],delimiter=',',names=True,dtype=None,comments='#',missing_values='None')
-	infile2 = genfromtxt(properties['infile2'],delimiter=',',names=True,dtype=None,comments='#',missing_values='None')
-	x_data1,y_data1 = infile1[properties['x1,y1'].split(',')[0]],infile1[properties['x1,y1'].split(',')[1]]
-	x_data2,y_data2 = infile2[properties['x2,y2'].split(',')[0]],infile2[properties['x2,y2'].split(',')[1]]
-
-	#++++++++++ LINE FORMAT ++++++++++
-	if properties['size'] != 'False': pyplot.figure(num=1,figsize=eval(properties['size']),dpi=eval(properties['dpi']))
-	else: pyplot.figure(num=1,dpi=eval(properties['dpi']))
+	if config.get('plot_format','size') != 'False': pyplot.figure(num=1,figsize=eval(config.get('plot_format','size')),dpi=eval(config.get('plot_format','dpi')))
+	else: pyplot.figure(num=1,dpi=eval(config.get('plot_format','dpi')))
 	pyplot.clf()
 	pyplot.subplot(1,1,1)
-	p1, = pyplot.plot(x_data1,y_data1,marker=properties['marker1'],markersize=int(properties['markersize1']),markeredgewidth=float(properties['markeredgewidth1']),linestyle=properties['linestyle1'],color=array(eval(properties['color1']))/255,alpha=float(properties['alpha1']))
-	p2, = pyplot.plot(x_data2,y_data2,marker=properties['marker2'],markersize=int(properties['markersize2']),markeredgewidth=float(properties['markeredgewidth2']),linestyle=properties['linestyle2'],color=array(eval(properties['color2']))/255,alpha=float(properties['alpha2']))
+
+	#++++++++++ PLOT ++++++++++
+	data_range = range(len(eval(config.get('data','infiles'))))
+	for i in data_range:
+		infile = genfromtxt(eval(config.get('data','infiles'))[i],delimiter=',',names=True,dtype=None,comments='#',missing_values='None')
+		x_data,y_data = infile[eval(config.get('data','columns'))[i][0]],infile[eval(config.get('data','columns'))[i][1]]
+
+		if config.get('axis_format','logx') == 'True' and config.get('axis_format','logy') == 'True':
+			p, = pyplot.loglog(x_data,y_data,marker=eval(config.get('line_format','marker'))[i],markersize=eval(config.get('line_format','markersize'))[i],markeredgewidth=eval(config.get('line_format','markeredgewidth'))[i],linestyle=eval(config.get('line_format','linestyle'))[i],color=array(eval(config.get('line_format','color'))[i])/255,alpha=eval(config.get('line_format','alpha'))[i])
+		elif config.get('axis_format','logx') == 'True':
+			p, = pyplot.semilogx(x_data,y_data,marker=eval(config.get('line_format','marker'))[i],markersize=eval(config.get('line_format','markersize'))[i],markeredgewidth=eval(config.get('line_format','markeredgewidth'))[i],linestyle=eval(config.get('line_format','linestyle'))[i],color=array(eval(config.get('line_format','color'))[i])/255,alpha=eval(config.get('line_format','alpha'))[i])
+		elif config.get('axis_format','logy') == 'True':
+			p, = pyplot.semilogy(x_data,y_data,marker=eval(config.get('line_format','marker'))[i],markersize=eval(config.get('line_format','markersize'))[i],markeredgewidth=eval(config.get('line_format','markeredgewidth'))[i],linestyle=eval(config.get('line_format','linestyle'))[i],color=array(eval(config.get('line_format','color'))[i])/255,alpha=eval(config.get('line_format','alpha'))[i])
+		else:
+			p, = pyplot.plot(x_data,y_data,marker=eval(config.get('line_format','marker'))[i],markersize=eval(config.get('line_format','markersize'))[i],markeredgewidth=eval(config.get('line_format','markeredgewidth'))[i],linestyle=eval(config.get('line_format','linestyle'))[i],color=array(eval(config.get('line_format','color'))[i])/255,alpha=eval(config.get('line_format','alpha'))[i])
 
 	#++++++++++ PLOT FORMAT ++++++++++
-	pyplot.suptitle(properties['suptitle'],fontsize=16)
-	pyplot.figtext(0.5,0.05,properties['x_label'],ha='center',va='center')
-	pyplot.figtext(0.05,0.5,properties['y_label'],rotation=90,ha='center',va='center')
+	pyplot.suptitle(config.get('plot_format','suptitle'),fontsize=16)
+	pyplot.figtext(0.5,0.05,config.get('axis_format','x_label'),ha='center',va='center')
+	pyplot.figtext(0.05,0.5,config.get('axis_format','y_label'),rotation=90,ha='center',va='center')
 	pyplot.xticks(fontsize=8)
 	pyplot.yticks(fontsize=8)
-	if properties['x_lim'] != 'False':
-		xlim = eval(properties['x_lim'])
+	if config.get('axis_format','x_lim') != 'False':
+		xlim = eval(config.get('axis_format','x_lim'))
 		pyplot.xlim(xlim[0],xlim[1])
-	if properties['y_lim'] != 'False':
-		ylim = eval(properties['y_lim'])
+	if config.get('axis_format','y_lim') != 'False':
+		ylim = eval(config.get('axis_format','y_lim'))
 		pyplot.ylim(ylim[0],ylim[1])
-	if properties['invert_x'] == 'True': pyplot.gca().invert_xaxis()
-	if properties['invert_y'] == 'True': pyplot.gca().invert_yaxis()
-	if properties['grid'] != 'False': pyplot.grid(b='on',which='major',axis=properties['grid'])
-	if properties['equalize_grid'] == 'True': pyplot.gca().set_aspect('equal', adjustable='box')
-	if properties['legend'] != 'False': pyplot.legend([p1,p2],eval(properties['legend']),loc=eval(properties['legend_loc']))
+	if config.get('axis_format','invert_x') == 'True': pyplot.gca().invert_xaxis()
+	if config.get('axis_format','invert_y') == 'True': pyplot.gca().invert_yaxis()
+	if config.get('axis_format','grid') != 'False': pyplot.grid(b='on',which='major',axis=config.get('axis_format','grid'))
+	if config.get('axis_format','equalize_grid') == 'True': pyplot.gca().set_aspect('equal', adjustable='box')
 
 	#++++++++ SAVE PLOT ++++++++
-	if properties['outfile'] == 'False': outfile = properties['infile'].split('.')[0]+'.pdf'
-	else: outfile = properties['outfile']
-	outpath = properties['outpath']
+	if config.get('output','outfile') == 'False': outfile = config.get('infile').split('.')[0]+'.pdf'
+	else: outfile = config.get('output','outfile')
+	outpath = config.get('output','outpath')
 	if not path.exists(outpath): mkdir(outpath)
 	pyplot.savefig('%s%s' %(outpath,outfile))
 	print('Plot saved to %s%s' %(outpath,outfile))
